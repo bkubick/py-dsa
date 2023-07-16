54f73b268f7d6d7ca6f5444224c58b8199ceda56
bkubick <bjk2158@columbia.edu>
2023-07-16 13:39:33 -0500

    Add initial implementation of BinaryTree
    Note that this is incomplete, and primarily touches on tree
    traversals and inserts. This does not include deleting values.

--- src/data_structures/__init__.py
+++ src/data_structures/__init__.py
@@ -1,5 +1,6 @@
 # coding: utf-8
+from .binary_tree import BinaryTree
 from .singly_linked_list import SinglyLinkedList
 
 
-__all__ = ['SinglyLinkedList']
+__all__ = ['BinaryTree', 'SinglyLinkedList']
---
+++ src/data_structures/binary_tree.py
@@ -0,0 +1,185 @@
+from __future__ import annotations
+
+from typing import Generic, List, Optional, TypeVar
+
+
+T = TypeVar('T')
+
+
+class BinaryTree(Generic[T]):
+    """ Binary tree implementation.
+    
+        Attributes:
+            _root (Optional[BinaryTree.Node]): Root node of tree.
+    """
+
+    class Node:
+        """ Node class for BinaryTree.
+
+            Attributes:
+                value (T): Value of node.
+                left (Optional[BinaryTree.Node]): Left node of current node.
+                right (Optional[BinaryTree.Node]): Right node of current node.
+        """
+        value: T
+        left: Optional[BinaryTree.Node]
+        right: Optional[BinaryTree.Node]
+
+        def __init__(self, value: BinaryTree.Node) -> None:
+            self.value = value
+            self.left = None
+            self.right = None
+
+        def __repr__(self) -> str:
+            """ String representation of node. """
+            left = self.left.value if self.left else None
+            right = self.right.value if self.right else None
+            return f'<Node value={self.value} left={left} right={right}>'       
+
+    _root: Optional[Node]
+
+    def __init__(self):
+        self._root = None
+
+    def __str__(self) -> str:
+        """ String representation of BinaryTree. """
+        return f'BinaryTree ({self.__repr__()})'
+
+    def __repr__(self) -> str:
+        """ String representation of BinaryTree. """
+        return str(self.in_order_traversal())
+
+    def _insert(self, value: T, node: BinaryTree.Node) -> None:
+        """ Insert value into tree helper function.
+        
+            Args:
+                value (T): Value to insert into tree.
+                node (BinaryTree.Node): Node to insert value into.
+        """
+        if node.value >= value:
+            if node.left is None:
+                node.left = BinaryTree.Node(value)
+            else:
+                self._insert(value, node=node.left)
+        else:
+            if node.right is None:
+                node.right = BinaryTree.Node(value)
+            else:
+                self._insert(value, node=node.right)
+
+    def insert(self, value: T) -> None:
+        """ Insert value into tree.
+        
+            Args:
+                value (T): Value to insert into tree.
+        """
+        if self._root is None:
+            self._root = BinaryTree.Node(value)
+        else:
+            self._insert(value, self._root)
+
+    def _in_order_traversal(self, node: Optional[BinaryTree.Node]) -> List[T]:
+        """ In order traversal helper function.
+        
+            Args:
+                node (Optional[BinaryTree.Node]): Node to traverse from.
+        """
+        if node is None:
+            return []
+
+        traversed_tree = []
+
+        traversed_tree.extend(self._in_order_traversal(node.left))
+        traversed_tree.append(node.value)
+        traversed_tree.extend(self._in_order_traversal(node.right))
+
+        return traversed_tree
+
+    def in_order_traversal(self) -> List[T]:
+        """ In order traversal of tree.
+
+            Example:
+                >>> tree = BinaryTree()
+                >>> tree.insert(5)
+                >>> tree.insert(3)
+                >>> tree.insert(7)
+                >>> tree.insert(2)
+                >>> tree.insert(4)
+                >>> tree.insert(6)
+                >>> tree.insert(8)
+                >>> tree.in_order_traversal()
+                [2, 3, 4, 5, 6, 7, 8]
+
+            Returns:
+                List[T]: In order traversal of tree.
+        """
+        return self._in_order_traversal(self._root)
+
+    def _pre_order_traversal(self, node: Optional[BinaryTree.Node]) -> List[T]:
+        """ Pre order traversal helper function.
+
+            Args:
+                node (Optional[BinaryTree.Node]): Node to traverse from.
+        """
+        if node is None:
+            return []
+
+        traversed_tree = [node.value]
+        traversed_tree.extend(self._pre_order_traversal(node.left))
+        traversed_tree.extend(self._pre_order_traversal(node.right))
+
+        return traversed_tree
+
+    def pre_order_traversal(self) -> List[T]:
+        """ Pre order traversal of tree.
+
+            Example:
+                >>> tree = BinaryTree()
+                >>> tree.insert(5)
+                >>> tree.insert(3)
+                >>> tree.insert(7)
+                >>> tree.insert(2)
+                >>> tree.insert(4)
+                >>> tree.insert(6)
+                >>> tree.insert(8)
+                >>> tree.pre_order_traversal()
+                [5, 3, 2, 4, 7, 6, 8]
+
+            Returns:
+                List[T]: Pre order traversal of tree.    
+        """
+        return self._pre_order_traversal(self._root)
+
+    def _post_order_traversal(self, node: Optional[BinaryTree.Node]) -> List[T]:
+        """ Post order traversal helper function.
+        
+            Args:
+                node (Optional[BinaryTree.Node]): Node to traverse from.
+        """
+        if node is None:
+            return []
+
+        traversed_tree = []
+
+        traversed_tree.extend(self._post_order_traversal(node.left))
+        traversed_tree.extend(self._post_order_traversal(node.right))
+        traversed_tree.append(node.value)
+
+        return traversed_tree
+
+    def post_order_traversal(self) -> List[T]:    
+        """ Post order traversal of tree.
+
+            Example:
+                >>> tree = BinaryTree()
+                >>> tree.insert(5)
+                >>> tree.insert(3)
+                >>> tree.insert(7)
+                >>> tree.insert(2)
+                >>> tree.insert(4)
+                >>> tree.insert(6)
+                >>> tree.insert(8)
+                >>> tree.post_order_traversal()
+                [2, 4, 3, 6, 8, 7, 5]
+        """
+        return self._post_order_traversal(self._root)
