8ed90d52e49ddd776d59a0c73d889dd70f5d611e
bkubick <bjk2158@columbia.edu>
2023-09-25 18:09:40 -0500

    Add Trie class

--- src/data_structures/__init__.py
+++ src/data_structures/__init__.py
@@ -2,6 +2,7 @@
 from .binary_tree import BinaryTree
 from .dynamic_array import DynamicArray
 from .singly_linked_list import SinglyLinkedList
+from .trie import Trie
 
 
-__all__ = ['BinaryTree', 'DynamicArray', 'SinglyLinkedList']
+__all__ = ['BinaryTree', 'DynamicArray', 'SinglyLinkedList', 'Trie']
---
+++ src/data_structures/trie.py
@@ -0,0 +1,82 @@
+# coding: utf-8
+
+from __future__ import annotations
+
+from collections import defaultdict
+
+
+__all__ = ['Trie']
+
+
+class _TrieNode(defaultdict):
+    """ A node in a trie.
+
+        Attributes:
+            is_word (bool): Whether or not the node represents a word.
+    """
+    is_word: bool
+
+    def __init__(self):
+        super().__init__(_TrieNode)
+        self.is_word = False
+
+
+class Trie:
+    """ A trie implementation.
+
+        Attributes:
+            _nodes (Dict[str, dict]): The nodes in the trie.
+    """
+
+    def __init__(self):
+        self._nodes: _TrieNode = _TrieNode()
+
+    def insert(self, word: str) -> None:
+        """ Inserts a word into the trie.
+
+            Args:
+                word (str): The word to insert.
+        """
+        current_mapping: _TrieNode = self._nodes
+        for letter in word:
+            current_mapping = current_mapping[letter]
+
+        current_mapping.is_word = True
+
+    def search(self, word: str) -> bool:
+        """ Searches for a word in the trie.
+        
+            Args:
+                word (str): The word to search for.
+        
+            Returns:
+                (bool) Whether or not the word is in the trie.
+        """
+        current_mapping: _TrieNode = self._nodes
+
+        for letter in word:
+            if letter not in current_mapping:
+                return False
+            
+            current_mapping = current_mapping[letter]
+
+        return current_mapping.is_word
+
+    def startsWith(self, prefix: str) -> bool:
+        """ Searches for a prefix in the trie.
+        
+            Args:
+                prefix (str): The prefix to search for.
+            
+            Returns:
+                (bool) Whether or not the prefix is in the trie.
+        """
+        current_mapping: _TrieNode = self._nodes
+
+        for letter in prefix:
+            if letter not in current_mapping:
+                return False
+            
+            current_mapping = current_mapping[letter]
+
+        return True
